c17036f83bc696473f4f0ea1cec71d16a3bf4523
wenbinai <208176146@qq.com>
2021-05-04 15:30:55 +0800

    初步完成线程不安全版阻塞队列

--- src/main/java/thread/synqueue/BlockingQueue.java
+++ src/main/java/thread/synqueue/BlockingQueue.java
@@ -1,4 +1,63 @@
 package thread.synqueue;
 
+/**
+ * 从零开始实现自己的阻塞队列
+ */
 public class BlockingQueue {
+    private Object[] items;
+    private int count;
+    private int putIndex;
+    private int takeIndex;
+
+    public BlockingQueue(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException();
+        }
+        items = new Object[capacity];
+        count = 0;
+        putIndex = 0;
+        takeIndex = 0;
+    }
+
+    public void put(Object item) throws InterruptedException {
+        while (true) {
+            if (count != items.length) {
+                enqueue(item);
+                break;
+            }
+            System.out.println("队列已满, 还不能生产");
+            Thread.sleep(200L);
+        }
+
+
+    }
+
+    public Object take() throws InterruptedException {
+        while (true) {
+            if (count != 0) {
+                return dequeue();
+            }
+            System.out.println("队列为空, 还不能消费");
+            Thread.sleep(200L);
+        }
+    }
+
+    private void enqueue(Object item) {
+        items[putIndex] = item;
+
+        if (++putIndex == items.length) {
+            putIndex = 0;
+        }
+        count++;
+    }
+
+    private Object dequeue() {
+        Object item = items[takeIndex];
+        items[takeIndex] = null;
+        if (++takeIndex == items.length) {
+            takeIndex = 0;
+        }
+        count--;
+        return item;
+    }
 }
--- src/main/java/thread/synqueue/Test.java
+++ src/main/java/thread/synqueue/Test.java
@@ -1,4 +1,49 @@
 package thread.synqueue;
 
 public class Test {
+    public static void main(String[] args) {
+        BlockingQueue q = new BlockingQueue(5);
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Producer(q)).start();
+            new Thread(new Customer(q)).start();
+        }
+
+    }
+}
+
+class Producer implements Runnable {
+    BlockingQueue queue;
+
+    public Producer(BlockingQueue q) {
+        queue = q;
+    }
+
+    public void run() {
+        for (int i = 0; ; i++) {
+            try {
+                queue.put(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Customer implements Runnable {
+    BlockingQueue queue;
+
+    public Customer(BlockingQueue q) {
+        queue = q;
+    }
+
+    public void run() {
+        for (int i = 0; ; i++) {
+            try {
+                Integer take = (Integer) queue.take();
+                System.out.println(take);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
