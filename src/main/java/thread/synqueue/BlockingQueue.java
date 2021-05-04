498b7e5be46b06be4e697fa2fbaadf71df02a73d
wenbinai <208176146@qq.com>
2021-05-04 18:51:46 +0800

    通过拆分锁的方式进一步提高并发执行效率

--- src/main/java/thread/synqueue/BlockingQueue.java
+++ src/main/java/thread/synqueue/BlockingQueue.java
@@ -1,5 +1,6 @@
 package thread.synqueue;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,56 +9,75 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class BlockingQueue {
     private Object[] items;
-    private int count;
+    private AtomicInteger count;
     private int putIndex;
     private int takeIndex;
 
-    private final ReentrantLock lock = new ReentrantLock();
-    private final Condition notFull = lock.newCondition();
-    private final Condition notEmpty = lock.newCondition();
+    private final ReentrantLock putLock = new ReentrantLock();
+    private final Condition notFull = putLock.newCondition();
+
+    private final ReentrantLock takeLock = new ReentrantLock();
+    private final Condition notEmpty = takeLock.newCondition();
 
     public BlockingQueue(int capacity) {
         if (capacity <= 0) {
             throw new IllegalArgumentException();
         }
         items = new Object[capacity];
-        count = 0;
+        count = new AtomicInteger(0);
         putIndex = 0;
         takeIndex = 0;
     }
 
     public void put(Object item) throws InterruptedException {
-        lock.lockInterruptibly();
+        putLock.lockInterruptibly();
         try {
-            while (count == items.length) {
+            while (count.get() == items.length) {
                 System.out.println("队列已满, 还不能生产");
                 notFull.await();
             }
             enqueue(item);
-            // 唤醒消费者线程
-            notEmpty.signalAll();
         } finally {
-            lock.unlock();
+            putLock.unlock();
+        }
+        // 唤醒消费者线程 (防止死锁)
+        signNotEmpty();
+    }
+
+    private void signNotEmpty() {
+        takeLock.lock();
+        try {
+            notEmpty.signal();
+        } finally {
+            takeLock.unlock();
         }
     }
 
+
     public Object take() throws InterruptedException {
-        lock.lockInterruptibly();
+        Object item;
+        takeLock.lockInterruptibly();
         try {
-            while (count == 0) {
+            while (count.get() == 0) {
                 System.out.println("队列为空, 还不能消费");
                 notEmpty.await();
             }
-
-            Object item = dequeue();
-            // 唤醒所有的消费者线程
-            notFull.signalAll();
-            return item;
+            item = dequeue();
         } finally {
-            lock.unlock();
+            takeLock.unlock();
         }
+        // 唤醒所有的消费者线程 防止死锁
+        signNotFull();
+        return item;
+    }
 
-
+    private void signNotFull() {
+        putLock.lock();
+        try {
+            notFull.signal();
+        } finally {
+            putLock.unlock();
+        }
     }
 
     private void enqueue(Object item) {
@@ -66,7 +86,7 @@ public class BlockingQueue {
         if (++putIndex == items.length) {
             putIndex = 0;
         }
-        count++;
+        count.getAndIncrement();
     }
 
     private Object dequeue() {
@@ -75,7 +95,7 @@ public class BlockingQueue {
         if (++takeIndex == items.length) {
             takeIndex = 0;
         }
-        count--;
+        count.getAndDecrement();
         return item;
     }
 }
--- src/main/java/thread/synqueue/Test2.java
+++ src/main/java/thread/synqueue/Test2.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Test2 {
     public static void main(String[] args) throws InterruptedException {
         final BlockingQueue q = new BlockingQueue(2);
-        final int threads = 400;
-        final int times = 100;
+        final int threads = 2000;
+        final int times = 400;
         List<Thread> threadList = new ArrayList<Thread>(threads * 2);
         long startTime = System.currentTimeMillis();
 
